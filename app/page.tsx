74610981b50cb071582f58db6231eb4be4bb934d
Sandile88 <sandilep.mremi@gmail.com>
2025-02-06 20:48:01 +0200

    trying to fix the message sending in deployement

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Education from "./components/Education";
-import Experience from "./components/Experience";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
 import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+import About from "./components/About";
 import Services from "./components/Services";
+import Experience from "./components/Experience";
+import Education from "./components/Education";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
